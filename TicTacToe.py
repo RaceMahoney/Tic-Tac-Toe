a7874efabddd2887146017ba89a40c90b7fa0cb8
Race <race.mahoney@gmail.com>
2017-11-05 17:32:06 -0500

    Updated print to Python 3

--- TicTacToe.py
+++ TicTacToe.py
@@ -3,34 +3,33 @@ from __future__ import print_function
 from IPython.display import clear_output
 #create the layout of the tic tac tow board using a list
 def display_board(board):
-    print
-    print ' - - - - - - - - - '
-    print '| ', board[0], ' | ', board[1], ' | ', board[2], '|'
-    print ' - - - - - - - - - '
-    print '| ', board[3], ' | ', board[4], ' | ', board[5], '|'
-    print ' - - - - - - - - - '
-    print '| ', board[6], ' | ', board[7], ' | ', board[8], '|'
-    print ' - - - - - - - - - '
+    print (' - - - - - - - - - ')
+    print ('| ', board[0], ' | ', board[1], ' | ', board[2], '|')
+    print (' - - - - - - - - - ')
+    print ('| ', board[3], ' | ', board[4], ' | ', board[5], '|')
+    print (' - - - - - - - - - ')
+    print ('| ', board[6], ' | ', board[7], ' | ', board[8], '|')
+    print (' - - - - - - - - - ')
 
 def player_input():
     #loop until the player selects the correct marker
     #find out what player one wants to be, then assign the other choice to player 2
     while True:
-        print 'What marker does player 1 want [X] or [O]'
+        print ('What marker does player 1 want [X] or [O]')
         answer = raw_input()
         if answer == 'X' or 'x':
-            print '\nPlayer one is [X]'
-            print 'Player two is [O]'
+            print ('\nPlayer one is [X]')
+            print ('Player two is [O]')
             P1_marker = 'X'
             break
         if answer == 'O' or 'o':
-            print '\nPlayer one is [O]'
-            print 'Player two is [X]'
+            print ('\nPlayer one is [O]')
+            print ('Player two is [X]')
             P1_marker = 'O'
             break
         #if the correct input was not entered, ask again
         else:
-            print 'Incorrect input. Try Again'
+            print ('Incorrect input. Try Again')
             continue
     return P1_marker
 
@@ -42,7 +41,7 @@ def place_marker(board, marker, position):
         board.insert(int(position)-1, marker)
         board.remove(position)
     else:
-        print 'Not in range'
+        print ('Not in range')
 
 def win_check(board,mark):
         #create a matrix of rows
@@ -86,13 +85,13 @@ def choose_first():
 def space_check(board, position):
     #determine if a player has placed a marker in that space yet or not
     if 'X' in board[int(position)-1]:
-        print '\nBad space.'
-        print 'Choose again'
+        print ('\nBad space.')
+        print ('Choose again')
         return False
 
     if 'O' in board[int(position)-1]:
-        print '\nBad space.'
-        print 'Choose again'
+        print ('\nBad space.')
+        print ('Choose again')
         return False
 
     return True
@@ -110,12 +109,12 @@ def player_choice(board):
     position = raw_input()
     #determine if the input is in the desired range or not
     if position in range(1,10):
-        print 'Choose a number between 1 and 9'
+        print ('Choose a number between 1 and 9')
     return position
 
 def replay():
     #ask to play the game again come completed
-    print 'Do you want play again? (y/n)'
+    print ('Do you want play again? (y/n)')
     answer = raw_input()
     if answer == 'y':
         return True
@@ -144,8 +143,8 @@ while game_on:
 
     if P1_first == True:
         #Player1's turn
-        print "Player 1's Turn"
-        print 'Choose location on board'
+        print ("Player 1's Turn")
+        print ('Choose location on board')
 
         #loop until a marker is correctly placed on the board
         while True:
@@ -162,7 +161,7 @@ while game_on:
 
         #check if player 1 has won
         if win_check(board, P1_marker) == True:
-            print "Player 1 WINS!"
+            print ("Player 1 WINS!")
             #ask to replay the game
             if replay():
                 board = ['1','2','3','4','5','6','7','8','9'] #reset board
@@ -174,8 +173,8 @@ while game_on:
 
     if P1_first == False:
         # Player2's turn.
-        print "Player 2's Turn"
-        print 'Choose location on board'
+        print ("Player 2's Turn")
+        print ('Choose location on board')
 
         #loop until a marker is correctly placed on the board
         while True:
@@ -190,7 +189,7 @@ while game_on:
         P1_first = True
         #check if player 1 has won
         if win_check(board, P2_marker) == True:
-            print "Player 2 WINS!"
+            print ("Player 2 WINS!")
             #ask to replay the game
             if replay():
                 board = ['1','2','3','4','5','6','7','8','9'] #reset board
@@ -202,8 +201,8 @@ while game_on:
 ####################################################################################
     #determine if board is full
     if full_board_check(board) == True:
-        print 'Board is full'
-        print 'Cat game'
+        print ('Board is full')
+        print ('Cat game')
         #ask to play again
         if replay():
             board = ['1','2','3','4','5','6','7','8','9'] #reset board
@@ -212,4 +211,4 @@ while game_on:
             game_on = False
             break
 
-print '\nThanks for planying!'
+print ('\nThanks for planying!')
